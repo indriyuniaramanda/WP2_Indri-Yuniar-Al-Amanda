c52c3f5880cd61648afb4272a97134f18818c577
indriyuniaramanda <aaalamanda@gmail.com>
2022-03-20 23:06:08 +0700

    menambahkan contoh1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Indri Yuniar Al'Amanda
+            saya tinggal di daerah Tangerang
+            olahraga yang saya sukai adalah
+            bulutangkis";
+    }
+}
